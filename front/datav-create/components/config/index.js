4057c960ea7a83f47ee24a28d3ae1d240fe53860
yulucui <yulucui@aliyun.com>
2021-09-21 23:12:31 +0800

    调整demo组件

--- front/datav-create/components/config/index.js
+++ front/datav-create/components/config/index.js
@@ -8,25 +8,8 @@
  * Modified By:
  */
 import React from 'react'
-import general from "./general";
-import antv from "./antv";
-import word from "./word";
-import material from "./material";
-import interactive from "./interactive";
-import custom from "./custom";
 import demo from "./demo";
-import geo from "./geo";
-import hidden from "./hidden";
 
 export default {
-    general: general,
-    antv: antv,
-    geo: geo,
-    word: word,
-    material: material,
-    interactive: interactive,
-    custom: custom,
-    demo: demo,
-
-    hidden: hidden //该目录下组件不在列表中显示，可作为子组件使用
+    demo: demo
 }
